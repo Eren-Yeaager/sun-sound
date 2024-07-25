04ebd287783efc8567cc8057fc7c1aea836c009f
Marpureddy Saswat <saswatmarpureddy@gmail.com>
2024-07-26 00:41:45 +0530

    home-page-feature-v1

--- app/components/sideBar/page.tsx
+++ app/components/sideBar/page.tsx
@@ -1,6 +1,16 @@
+"use client";
 import { HomeIcon, Music, BookOpen, Coins, UserRoundPen } from "lucide-react";
 import Link from "next/link";
+import { usePathname, useRouter } from "next/navigation";
+import { useEffect } from "react";
+
 export default function SideBar() {
+  const router = useRouter();
+
+  useEffect(() => {
+    router.push("/home");
+  }, [router]);
+
   return (
     <div className="bg-darkerGray h-full w-64 p-6 text-white fixed">
       <div className="flex items-center mb-6">
@@ -8,36 +18,34 @@ export default function SideBar() {
       </div>
       <nav>
         <ul className="space-y-4">
-          <li className="flex items-center p-2  hover:bg-darkGray rounded-lg hover:text-neonPink transition-colors duration-300 cursor-pointer">
+          <li className="flex items-center p-2 rounded hover:bg-darkGray hover:text-neonPink transition-colors duration-300 cursor-pointer">
             <HomeIcon />
             <span className="ml-2">
-              <Link href={"/home"}>Home</Link>
+              <Link href="/home">Home</Link>
             </span>
           </li>
           <li className="flex items-center p-2 rounded hover:bg-darkGray hover:text-neonPink transition-colors duration-300 cursor-pointer">
             <Music />
             <span className="ml-2">
-              <Link href={"/collection"}>Music</Link>
+              <Link href="/collection">Music</Link>
             </span>
           </li>
           <li className="flex items-center p-2 rounded hover:bg-darkGray hover:text-neonPink transition-colors duration-300 cursor-pointer">
             <BookOpen />
             <span className="ml-2">
-              <Link href={"/courses"}>Courses</Link>
+              <Link href="/courses">Courses</Link>
             </span>
           </li>
           <li className="flex items-center p-2 rounded hover:bg-darkGray hover:text-neonPink transition-colors duration-300 cursor-pointer">
             <Coins />
             <span className="ml-2">
-              {" "}
-              <Link href={"/tokenomics"}>Tokenomics</Link>
+              <Link href="/tokenomics">Tokenomics</Link>
             </span>
           </li>
           <li className="flex items-center p-2 rounded hover:bg-darkGray hover:text-neonPink transition-colors duration-300 cursor-pointer">
             <UserRoundPen />
             <span className="ml-2">
-              {" "}
-              <Link href={"/profile"}>Profile</Link>
+              <Link href="/profile">Profile</Link>
             </span>
           </li>
         </ul>
--- app/home/page.tsx
+++ app/home/page.tsx
@@ -1,7 +1,20 @@
+import { Input } from "@/components/ui/input";
+import { Button } from "@/components/ui/button";
 export default function Home() {
   return (
-    <div className="bg-darkGray min-h-screen flex">
-      <div className="text-2xl text-white">Home</div>
+    <div className="bg-darkGray min-h-screen flex items-start justify-center gap-4">
+      <div className="flex items-start gap-4 mt-10 w-full max-w-xl">
+        <Input className="flex-grow" />
+
+        <Button variant="outline" size="lg">
+          Filters
+        </Button>
+      </div>
+      <div className="flex items-center mt-10 ml-20">
+        <Button variant="default" size="lg">
+          Login
+        </Button>
+      </div>
     </div>
   );
 }
